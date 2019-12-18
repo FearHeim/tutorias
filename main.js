67a8e2710a83a951ebdfc45dc9b1c829a59e57e3
FearHeim <narjos2000@gmail.com>
2019-12-18 00:06:30 -0500

    gamer2

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 //Pagina inicial
+let datoMujeres;
+let datoHombres;
+let TotalPorcentaje;
+
+datoHombres=80;
+datoMujeres=20;
+
+
+function Porcentaje(){
+    TotalPorcentaje=datoHombres+datoMujeres;
+    var porcentHombres= (datoHombres / TotalPorcentaje)*100;
+    var porcentMujeres= (datoMujeres / TotalPorcentaje)*100;
+    console.log("Asistieron "+datoMujeres+ " mujeres al curso, que corresponde al " +porcentMujeres+ " % de las asistencias totales")
+    console.log("Asistieron "+datoHombres+ " hombres al curso, que corresponde al " +porcentHombres+ " % de las asistencias totales")
+}
+
+Porcentaje();
