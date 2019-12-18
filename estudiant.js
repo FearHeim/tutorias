696ff31f45a040f094b30ea62aaed83ef2abd219
FearHeim <narjos2000@gmail.com>
2019-12-18 00:29:00 -0500

    let

--- estudiant.js
+++ estudiant.js
@@ -1,4 +1,5 @@
 //Pagina de porcentaje
+require; 
 let datoMujeres;
 let datoHombres;
 let TotalPorcentaje;
@@ -9,8 +10,8 @@ datoMujeres=20;
 
 function Porcentaje(){
     TotalPorcentaje=datoHombres+datoMujeres;
-    var porcentHombres= (datoHombres / TotalPorcentaje)*100;
-    var porcentMujeres= (datoMujeres / TotalPorcentaje)*100;
+    let porcentHombres= (datoHombres / TotalPorcentaje)*100;
+    let porcentMujeres= (datoMujeres / TotalPorcentaje)*100;
     console.log("Asistieron "+datoMujeres+ " mujeres al curso, que corresponde al " +porcentMujeres+ " % de las asistencias totales")
     console.log("Asistieron "+datoHombres+ " hombres al curso, que corresponde al " +porcentHombres+ " % de las asistencias totales")
     let mensaje;
