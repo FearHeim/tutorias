c464dc1bf83281d4c9b4516eaa82bb29f3927fa6
FearHeim <narjos2000@gmail.com>
2019-12-18 00:29:59 -0500

    MMV

--- estudiant.js
+++ estudiant.js
@@ -14,8 +14,8 @@ function Porcentaje(){
     let porcentMujeres= (datoMujeres / TotalPorcentaje)*100;
     console.log("Asistieron "+datoMujeres+ " mujeres al curso, que corresponde al " +porcentMujeres+ " % de las asistencias totales")
     console.log("Asistieron "+datoHombres+ " hombres al curso, que corresponde al " +porcentHombres+ " % de las asistencias totales")
-    let mensaje;
-    mensaje=("Asistieron "+datoMujeres+ " mujeres al curso, que corresponde al " +porcentMujeres+ " % de las asistencias totales")
+    //let mensaje;
+    //mensaje=("Asistieron "+datoMujeres+ " mujeres al curso, que corresponde al " +porcentMujeres+ " % de las asistencias totales")
 }
 
 Porcentaje();
