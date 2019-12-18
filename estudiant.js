63ca4191aa992cf2cbe3b20f07cac0911fff1700
FearHeim <narjos2000@gmail.com>
2019-12-18 00:23:59 -0500

    nose3

--- estudiant.js
+++ estudiant.js
@@ -13,6 +13,8 @@ function Porcentaje(){
     var porcentMujeres= (datoMujeres / TotalPorcentaje)*100;
     console.log("Asistieron "+datoMujeres+ " mujeres al curso, que corresponde al " +porcentMujeres+ " % de las asistencias totales")
     console.log("Asistieron "+datoHombres+ " hombres al curso, que corresponde al " +porcentHombres+ " % de las asistencias totales")
+    let mensaje;
+    mensaje=("Asistieron "+datoMujeres+ " mujeres al curso, que corresponde al " +porcentMujeres+ " % de las asistencias totales")
 }
 
 Porcentaje();
