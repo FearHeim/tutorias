ca7bd59cbec321fa9499804e5be904e0df0f5387
FearHeim <narjos2000@gmail.com>
2020-01-12 22:49:22 -0500

    gamer

--- public/JS/auth/authController.js
+++ public/JS/auth/authController.js
@@ -10,11 +10,21 @@ $(() => {
         auth.crearCuentaEmailPass(email, password, nombres)
     });
 
-    $("#button").click(() => {
+    $("#button1").click(() => {
         const email = $('#inputlogin').val();
         const password = $('#inputcontra').val();
         const auth = new Autenticacion()
         auth.autEmailPass(email, password)
+    });
+
+    $('#btnRegistrarse').click(() => {
+        $('#modalSesion').modal('close');
+        $('#modalRegistro').modal('open');        
+    });
+
+    $('#button').click(() => {
+        $('#modalRegistro').modal('close');
+        $('#modalSesion').modal('open');
     });
 
     
